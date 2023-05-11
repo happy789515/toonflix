fac8d85268a1867faad7355e7cb37b18ed2c3118
SangWon <ondeal@modutech.co.kr>
2023-05-11 18:17:18 +0900

    Buttons Section

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class App extends StatelessWidget {
           body: Padding(
             padding: EdgeInsets.symmetric(horizontal: 40),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 SizedBox(
                   height: 80,
@@ -33,12 +34,59 @@ class App extends StatelessWidget {
                             fontWeight: FontWeight.w800,
                           ),
                         ),
-                        Text('Welcome back',
-                            style: TextStyle(
-                                color: Colors.white.withOpacity(0.5),
-                                fontSize: 18)),
+                        Text(
+                          'Welcome back',
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.5),
+                            fontSize: 18,
+                          ),
+                        ),
                       ],
                     )
+                  ],
+                ),
+                SizedBox(
+                  height: 120,
+                ),
+                Text(
+                  'Total Balance',
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.8),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  '\$5 194 482',
+                  style: TextStyle(
+                    fontSize: 42,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(45),
+                      ),
+                      child: Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 20,
+                            horizontal: 50,
+                          ),
+                          child: Text(
+                            'Transfer',
+                            style: TextStyle(
+                              fontSize: 22,
+                            ),
+                          )),
+                    ),
                   ],
                 )
               ],
